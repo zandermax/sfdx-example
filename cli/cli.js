4181286c16212bb446db35a34cf4d8d082d8e5c8
Zander Maxwell <zander.maxwell@reedhein.com>
2018-01-15 13:25:59 -0800

    Updated default command, removed comments and unused node modules.

--- cli/cli.js
+++ cli/cli.js
@@ -1,23 +1,34 @@
 #! /usr/bin/env node
 
-const config = require('../config/config')
 const sfdx = require('sfdx')
-const shell = require('shelljs')
 const yargs = require('yargs')
-
-// sfdx.project.setProjectPath(config.projectPath)
 
 yargs.command(
   '$0 [orgname] [alias|org|a]',
   'Performs SFDX commands. By default, this command lists the status of a connected org.',
   yargs => {
-    yargs.positional('orgname', {
-      describe: 'Alias of the org of which to check status'
-    })
-    yargs.option('alias', {
-      alias: ['a', 'org'],
-      describe: 'Alias of the org of which to check status'
-    })
+    yargs
+      .positional('orgname', {
+        describe: 'Alias of the org of which to check status'
+      })
+      .option('alias', {
+        alias: ['org', 'a'],
+        describe: 'Alias of the org of which to check status'
+      })
+      .option('remote', {
+        alias: ['r'],
+        describe: 'Fetch only changes made remotely',
+        conflicts: 'local',
+        type: 'boolean'
+      })
+      .option('local', {
+        alias: ['l'],
+        describe: 'Fetch only changes in code locally',
+        conflicts: 'remote',
+        type: 'boolean'
+      })
+      .example('$0 status MyOrg', "- Gets the local and remote code status of 'MyOrg'")
+      .example('$0 status --remote -a MyOrg', "- Gets the remote code status of 'MyOrg'")
   },
   argv => {
     argv.alias = argv.alias || argv.orgname
